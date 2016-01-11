08f2b35d5aad42a4b33bcca4c74f3df9e6121413
tmharkins <tmharkins@gmail.com>
2016-01-11 23:56:53 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,32 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+	def initialize(input)
+		@input = input
+		@sum_of_squares = 0
+		@square_of_sum = 0
+		@answer = 0
+		@counter = 0
+	end
+
+  def difference
+		counter = 1
+		until counter > @input
+			get_square_of_sum(counter)
+			get_sum_of_squares(counter)
+			counter += 1
+		end
+    answer
+		return @answer
+	end
+
+	def get_square_of_sum(counter)
+		@square_of_sum = @square_of_sum + counter
+	end
+
+	def get_sum_of_squares(counter)
+		@sum_of_squares = @sum_of_squares + counter**2
+	end
+
+  def answer
+		@answer = (@square_of_sum**2) - @sum_of_squares
+	end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,15 @@
-# Implement your procedural solution here!
+def sum_square_difference(number)
+	counter = 1
+	sum_of_squares = 0
+	square_of_sum = 0
+	difference = 0
+  until counter > number
+		square_of_sum = square_of_sum + counter
+		sum_of_squares = sum_of_squares + counter**2
+		counter += 1
+	end
+
+	square_of_sum = square_of_sum ** 2
+	difference = square_of_sum - sum_of_squares
+  return difference
+end
